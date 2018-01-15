a2911ae43387a50ff11cc1d3dfa17e8daa24a748
idaszakdaniel <idaszak1@gmail.com>
2018-01-15 12:34:20 +0100

    muscle model added to stats

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserSettingsService } from '../../services/user-settings.service';
+
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
@@ -7,9 +9,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatsComponent implements OnInit {
 
-  constructor() { }
+  weaknesses: string;
+  m0: any = {"visibility" : "hidden"};
+  m1: any = {"visibility" : "hidden"};
+  m2: any = {"visibility" : "hidden"};
+  m3: any = {"visibility" : "hidden"};
+  m4: any = {"visibility" : "hidden"};
+  m5: any = {"visibility" : "hidden"};
+  m6: any = {"visibility" : "hidden"};
+  m7: any = {"visibility" : "hidden"};
+  m8: any = {"visibility" : "hidden"};
+  m9: any = {"visibility" : "hidden"};
+  m10: any = {"visibility" : "hidden"};
+  m11: any = {"visibility" : "hidden"};
+  m12: any = {"visibility" : "hidden"};
+  userSettings;
+
+  constructor(userSettingsService: UserSettingsService) {
+    this.userSettings = userSettingsService;
+  }
 
   ngOnInit() {
+    this.weaknesses = this.userSettings.getWeaknesses();
+    this.m0 = {"visibility" : "hidden"};
+    this.m1 = {"visibility" : "hidden"};
+    this.m2 = {"visibility" : "hidden"};
+    this.m3 = {"visibility" : "hidden"};
+    this.m4 = {"visibility" : "hidden"};
+    this.m5 = {"visibility" : "hidden"};
+    this.m6 = {"visibility" : "hidden"};
+    this.m7 = {"visibility" : "hidden"};
+    this.m8 = {"visibility" : "hidden"};
+    this.m9 = {"visibility" : "hidden"};
+    this.m10 = {"visibility" : "hidden"};
+    this.m11 = {"visibility" : "hidden"};
+    this.m12 = {"visibility" : "hidden"};
+
+    if(this.weaknesses == "bench"){
+      this.m0 = {"visibility" : "visible"};
+      this.m10 = {"visibility" : "visible"};
+      this.m11 = {"visibility" : "visible"};
+    }
+    if(this.weaknesses == "squat"){
+      this.m5 = {"visibility" : "visible"};
+      this.m3 = {"visibility" : "visible"};
+      this.m2 = {"visibility" : "visible"};
+      this.m1 = {"visibility" : "visible"};
+    }
+    if(this.weaknesses == "press"){
+      this.m12 = {"visibility" : "visible"};
+      this.m11 = {"visibility" : "visible"};
+      this.m8 = {"visibility" : "visible"};
+    }
+    if(this.weaknesses == "dl"){
+      this.m3 = {"visibility" : "visible"};
+      this.m4 = {"visibility" : "visible"};
+      this.m5 = {"visibility" : "visible"};
+      this.m6 = {"visibility" : "visible"};
+      this.m7 = {"visibility" : "visible"};
+      this.m9 = {"visibility" : "visible"};
+    }
   }
 
 }
--- src/app/services/user-settings.service.ts
+++ src/app/services/user-settings.service.ts
@@ -15,4 +15,19 @@ export class UserSettingsService {
     return this.settings;
   }
 
+  getWeaknesses(){
+    return this.compareLifts();
+  }
+
+  compareLifts(){
+    if(this.checkLifts(this.settings.squat, this.settings.bench, 75)) return "bench";
+    if(this.checkLifts(this.settings.squat, this.settings.press, 50)) return "press";
+    if(this.checkLifts(this.settings.dl, this.settings.squat, 115)) return "squat";
+    if(((this.settings.squat/this.settings.dl)*100) > 86) return "dl";
+  }
+
+  checkLifts(lift1, lift2, percentage){
+    return (((lift2/lift1)*100) < percentage) ? true : false;
+  }
+
 }
